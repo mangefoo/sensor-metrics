870ccc94e41e41cf925f0bb04f20fda7e7305575
Magnus Eriksson <eriksson.mag@gmail.com>
2025-01-20 15:40:00 +0100

    Disable HTTP connection reuse

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/prometheus/client_golang v1.9.0
 	github.com/prometheus/common v0.15.0
+	github.com/sirupsen/logrus v1.9.3 // indirect
 )
--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"os"
 	"strconv"
@@ -16,45 +16,53 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sirupsen/logrus"
 )
 
 type Sensor struct {
-	Name string
+	Name  string
 	State struct {
 		Temperature int
-		Status int
+		Status      int
 		LastUpdated string
-		Presence bool
-		LightLevel int
+		Presence    bool
+		LightLevel  int
 	}
 }
 
 type SensorReport struct {
-	Reporter string `json:"reporter"`
-	Topic string `json:"topic"`
-	Sensors map[string]string `json:"sensors"`
+	Reporter string            `json:"reporter"`
+	Topic    string            `json:"topic"`
+	Sensors  map[string]string `json:"sensors"`
+}
+
+var httpClient *http.Client = &http.Client{
+	Transport: &http.Transport{
+		DisableKeepAlives: true,
+	},
 }
 
 func initMetrics() {
 	go func() {
 		if config.PhilipsHueUrl == "" {
+			logrus.Info("Philips Hue URL not set")
 			return
 		}
 
 		for {
 			response, err := http.Get(config.PhilipsHueUrl + "/sensors")
 			if err != nil {
-				fmt.Println(err)
+				logrus.Errorf("Failed to get sensor data: %v", err)
 			} else {
-				bodyBytes, err := ioutil.ReadAll(response.Body)
+				bodyBytes, err := io.ReadAll(response.Body)
 				if err != nil {
-					fmt.Printf("%+v\n", err)
+					logrus.Errorf("Failed to read sensor response: %v", err)
 				}
 
 				var sensorResponse = make(map[string]Sensor)
 				err = json.Unmarshal(bodyBytes, &sensorResponse)
 				if err != nil {
-					fmt.Println(err)
+					logrus.Errorf("Unmarshal of sensor response failed: %v", err)
 				}
 
 				motionSensor := sensorResponse[config.MotionSensorId]
@@ -81,6 +89,7 @@ func initMetrics() {
 func sendSensorReport(temperatureSensor Sensor, motionSensor Sensor, lightLevelSensor Sensor) {
 
 	if config.SensorRelayUrl != "" {
+		retry := true
 		var sensorReport = SensorReport{
 			Reporter: "hue-sensor-agent",
 			Topic:    "sensors",
@@ -90,11 +99,24 @@ func sendSensorReport(temperatureSensor Sensor, motionSensor Sensor, lightLevelS
 				"hue_lightlevel":  fmt.Sprintf("%d", lightLevelSensor.State.LightLevel),
 			},
 		}
+
+		logrus.Debugf("Sending report to %v", config.SensorRelayUrl)
+
 		reportJson, _ := json.Marshal(sensorReport)
-		response, err := http.Post(config.SensorRelayUrl, "application/json", bytes.NewBuffer(reportJson))
+	retry:
+		response, err := httpClient.Post(config.SensorRelayUrl, "application/json", bytes.NewBuffer(reportJson))
 		if err != nil {
-			println("Failed to send report: " + response.Status)
+			if err == io.EOF && retry {
+				logrus.Info("Got EOF, retrying")
+				goto retry
+			}
+			logrus.Error("Failed to send report: " + err.Error())
+		} else {
+			defer response.Body.Close()
 		}
+
+	} else {
+		logrus.Infof("No sensor report URL configured")
 	}
 }
 
@@ -122,26 +144,30 @@ func initConfig(configPath string) {
 		panic(err)
 	}
 
-	strEnvMap := map[string]*string {
-		"PHILIPS_HUE_URL": &config.PhilipsHueUrl,
-		"MOTION_SENSOR_ID": &config.MotionSensorId,
+	strEnvMap := map[string]*string{
+		"PHILIPS_HUE_URL":       &config.PhilipsHueUrl,
+		"MOTION_SENSOR_ID":      &config.MotionSensorId,
 		"LIGHT_LEVEL_SENSOR_ID": &config.LightLevelSensorId,
 		"TEMPERATURE_SENSOR_ID": &config.TemperatureSensorId,
-		"SENSOR_RELAY_URL": &config.SensorRelayUrl,
+		"SENSOR_RELAY_URL":      &config.SensorRelayUrl,
 	}
 
 	for key, configRef := range strEnvMap {
 		value, exist := os.LookupEnv(key)
-		if exist { *configRef = value }
+		if exist {
+			*configRef = value
+		}
 	}
 
-	intEnvMap := map[string]*int {
+	intEnvMap := map[string]*int{
 		"PORT": &config.Port,
 	}
 
 	for key, configRef := range intEnvMap {
 		value, exist := os.LookupEnv(key)
-		if exist { *configRef, _ = strconv.Atoi(value) }
+		if exist {
+			*configRef, _ = strconv.Atoi(value)
+		}
 	}
 }
 
@@ -161,12 +187,12 @@ var (
 )
 
 type Config struct {
-	PhilipsHueUrl string
-	MotionSensorId string
-	LightLevelSensorId string
+	PhilipsHueUrl       string
+	MotionSensorId      string
+	LightLevelSensorId  string
 	TemperatureSensorId string
-	Port int
-	SensorRelayUrl string
+	Port                int
+	SensorRelayUrl      string
 }
 
 var config Config
@@ -180,6 +206,6 @@ func main() {
 	initMetrics()
 
 	http.Handle("/metrics", promhttp.Handler())
-	fmt.Printf("Listening to :%d\n", config.Port)
-	log.Fatal("%+v\n", http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil));
+	logrus.Infof("Listening to :%d\n", config.Port)
+	log.Fatal("%+v\n", http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil))
 }
